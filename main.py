f7b4f53b614d8371ae64d1df1e5ed5614de1a9c7
Pranav Mishra <62085897+PRANAV40@users.noreply.github.com>
2023-07-03 11:06:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-"""
-Create a script which will generate a report on Profit and loss statement of share market portfolio.
-Get input from the user in csv file. Csv file will have 3 columns Company Name, Rate per share, quantity of share.
-The script should make a call to BSE API and get the current/LTP (last trading prize) of that company share.
-Get the mapping of company name and the BSE stock code from the
-URL https://static.quandl.com/BSE+Descriptions/stocks.txt
-Generate a statement of Profit and Loss as well calculate the deviation from purchase price to current price.
-Log the data into another text file. The log data should contain the company name and day high price of that stock.
-You an use pandas for this assignment and connect the file to database and also make a sender of excel file using
-smtplib """
-
 # For import the csv file, make a log file and export the data into excel file
 import os.path
 import pandas as pd
